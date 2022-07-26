244e3e642014ec5f46ad72cd44e53847db9ec5db
kusi <lawcubegsm@gmail.com>
2022-07-26 13:57:25 +0000

    button double issue fix

--- js/toggle-menu.js
+++ js/toggle-menu.js
@@ -1,145 +1,98 @@
-function create_element(el, ids = "", cl = "") {
-    let doc = document.createElement(el)
-    
-    if (ids !== "") {
-        doc.id = ids
-    }
+function createMyElement(el, ids = '', cl = '') {
+  const doc = document.createElement(el);
 
-    if (cl !== "") {
-        doc.className = cl
-    }
-    return doc
+  if (ids !== '') {
+    doc.id = ids;
+  }
+
+  if (cl !== '') {
+    doc.className = cl;
+  }
+  return doc;
 }
 
-function create_menu() {
-    let container = create_element("div", "nav-toggle")
+function createMyMenu() {
+  const container = createMyElement('div', 'nav-toggle');
 
-    // create button
-    const toggle_btn_container = create_element('div', 'close-button-container')
-    const toggle_btn = create_element('img', 'img-close')
-    toggle_btn.setAttribute('src', './../images/x.svg')
-    toggle_btn_container.appendChild(toggle_btn)
-    container.appendChild(toggle_btn_container)
+  // create button
+  const toggle_btn_container = createMyElement('div', 'close-button-container');
+  const toggle_btn = createMyElement('img', 'img-close');
+  toggle_btn.setAttribute('src', './../images/x.svg');
+  toggle_btn_container.appendChild(toggle_btn);
+  container.appendChild(toggle_btn_container);
 
-    // create menu list
-    let ul = create_element('ul', "js_nav", 'js-nav');
-    let li, a;
-    let content = ["Portfolio", "About", "Contact"];
-    let inner_links = ["#my-works", "#me", "#contact"]
-    
-    // create individual li and aattact to ul
-    for (let i = 0; i < content.length; i++) {
-        li = create_element('li', "", 'js-nav-items')
-        a = create_element('a', content[i])
-        a.setAttribute('href', inner_links[i] );
-        a.textContent = content[i]
-        li.appendChild(a)
-        ul.appendChild(li)
-    }
-    // add ul to container
-    container.appendChild(ul)
+  // create menu list
+  const ul = createMyElement('ul', 'js_nav', 'js-nav');
+  let li; let
+    a;
+  const content = ['Portfolio', 'About', 'Contact'];
+  const innerLinks = ['#my-works', '#me', '#contact'];
 
-    //create buttom img
-    const toggle_img_container = create_element('div', 'menu-img')
-    const toggle_img = create_element('img')
-    toggle_img.setAttribute('src', './../images/Shape.png')
-    toggle_img_container.appendChild(toggle_img)
+  // create individual li and aattact to ul
+  for (let i = 0; i < content.length; i++) {
+    li = createMyElement('li', '', 'js-nav-items');
+    a = createMyElement('a', content[i]);
+    a.setAttribute('href', innerLinks[i]);
+    a.textContent = content[i];
+    li.appendChild(a);
+    ul.appendChild(li);
+  }
+  // add ul to container
+  container.appendChild(ul);
 
-    container.appendChild(toggle_img_container)
+  // create buttom img
+  const toggle_img_container = createMyElement('div', 'menu-img');
+  const toggle_img = createMyElement('img');
+  toggle_img.setAttribute('src', './../images/Shape.png');
+  toggle_img_container.appendChild(toggle_img);
 
-    // console.log(container)
-    return container
+  container.appendChild(toggle_img_container);
 
+  // console.log(container)
+  return container;
 }
 
 function insert_to_page() {
-    document.body.insertBefore(create_menu(), document.body.children[0])
-
+  document.body.insertBefore(createMyMenu(), document.body.children[0]);
 }
 
-function remove_element(el){
-    el.remove()
+function remove_element(el) {
+  el.remove();
 }
 
+const btn = document.querySelector('#toggle-menu');
+btn.addEventListener('click', function (event) {
+  this.style.display = 'none';
+  insert_to_page();
+  const first_rem = document.body.firstElementChild;
+  const img_close = document.getElementById('img-close');
 
+  event.stopPropagation();
+  const btn_mob = document.querySelector('#toggle-menu');
+  const links = document.querySelector('.js_nav li');
 
-btn = document.querySelector('#toggle-menu');
-btn.addEventListener("click", function(event){
-    this.style.display = "none";
-    insert_to_page();
-    let first_rem = document.body.firstElementChild
-    let img_close = document.getElementById('img-close')
-    // let click = true
-    event.stopPropagation()
-    
-    // if(!click) {
-    let links  = document.querySelector('.js_nav li');
-    img_close.addEventListener("click", function(){
-        // console.log(event.currentTarget)
-        remove_element(first_rem )
-        btn.style.display = "inline";
-        event.stopPropagation()
+  img_close.addEventListener('click', () => {
+    // console.log(event.currentTarget)
+    remove_element(first_rem);
+    btn_mob.style.display = 'inline';
+    event.stopPropagation();
+  });
 
-    })
-    
-    document.querySelector('#Portfolio').addEventListener("click", function(){
-        remove_element(first_rem )
-        btn.style.display = "inline";
-        event.stopPropagation()
-    })
-   
-    document.querySelector('#About').addEventListener("click", function(){
-        remove_element(first_rem )
-        btn.style.display = "inline";
-        event.stopPropagation()
-    })
-    document.querySelector('#Contact').addEventListener("click", function(){
-        remove_element(first_rem )
-        btn.style.display = "inline";
-        event.stopPropagation()
-    })
-})
+  document.querySelector('#Portfolio').addEventListener('click', () => {
+    remove_element(first_rem);
+    btn_mob.style.display = 'inline';
+    event.stopPropagation();
+  });
 
+  document.querySelector('#About').addEventListener('click', () => {
+    remove_element(first_rem);
+    btn_mob.style.display = 'inline';
+    event.stopPropagation();
+  });
 
-function remove_element(el){
-    el.remove()
-}
-
-
-
-btn = document.querySelector('#toggle-menu');
-btn.addEventListener("click", function(event){
-    this.style.display = "none";
-    insert_to_page();
-    let first_rem = document.body.firstElementChild
-    let img_close = document.getElementById('img-close')
-    // let click = true
-    event.stopPropagation()
-    
-    // if(!click) {
-    let links  = document.querySelector('.js_nav li');
-    img_close.addEventListener("click", function(){
-        // console.log(event.currentTarget)
-        remove_element(first_rem )
-        btn.style.display = "inline";
-        event.stopPropagation()
-
-    })
-    
-    document.querySelector('#Portfolio').addEventListener("click", function(){
-        remove_element(first_rem )
-        btn.style.display = "inline";
-        event.stopPropagation()
-    })
-   
-    document.querySelector('#About').addEventListener("click", function(){
-        remove_element(first_rem )
-        btn.style.display = "inline";
-        event.stopPropagation()
-    })
-    document.querySelector('#Contact').addEventListener("click", function(){
-        remove_element(first_rem )
-        btn.style.display = "inline";
-        event.stopPropagation()
-    })
-})
+  document.querySelector('#Contact').addEventListener('click', () => {
+    remove_element(first_rem);
+    btn_mob.style.display = 'inline';
+    event.stopPropagation();
+  });
+});
